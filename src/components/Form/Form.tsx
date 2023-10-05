cea96990b6c4cb3556fdb28366253854ca820711
AdrianCon <adrian.dev@tutanota.com>
2023-10-05 01:52:07 -0600

    Add domain, ip or range validation

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,13 +1,13 @@
 import './Form.scss'
 import Input from '../Input/Input';
 import Button from '../Button/Button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Select from '../Select/Select';
-import { wrap } from 'module';
 import ScanLoader from '../ScanLoader/ScanLoader';
 import Code from '../Code/Code';
 import CheckBoxes from '../CheckBoxes/CheckBoxes';
 import Summary from '../Summary/Summary';
+import { Alert } from '@mui/material';
 
 interface nmap{
     ip_address: string;
@@ -26,8 +26,40 @@ export default function Form() {
     const [result, setResult] = useState("");
     const [isLoading, setIsLoading] = useState(false);
     const [selectedVis, setSelectedVis] = useState('text');
+    const [error, setError] = useState(false);
+
+    function validateInput(input: string) {
+        const ValidIpAddressRegex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/\d{1,2})?$/;
+        const ValidHostnameRegex = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/;
+      
+        if (ValidIpAddressRegex.test(input)) {
+          return true;
+        } else if (ValidHostnameRegex.test(input)) {
+          return true;
+        } else {
+          return false;
+        }
+    }
+
+    useEffect(() => {
+        const timeId = setTimeout(() => {
+          // After 3 seconds set the show value to false
+          setError(false)
+        }, 8000)
+    
+        return () => {
+          clearTimeout(timeId)
+        }
+      }, [error]);
+
 
     const handleScanEvent = (e: React.MouseEvent<HTMLButtonElement>) => {
+        const validInput = validateInput(nmapScan.ip_address);
+        console.log(validInput)
+        if(!validInput) {
+            setError(true)
+            return
+        }
         var myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
 
@@ -133,6 +165,17 @@ export default function Form() {
                 {isLoading ? <ScanLoader/> : null}
                 {(result && selectedVis === 'text') ? <Summary data={JSON.parse(result)}/> : null}
                 {(result && selectedVis === 'json') ? <Code code={result}/> : null}
+                {error && (
+                    <div
+                        style={{
+                            position: 'sticky',
+                            width: '50%',
+                            bottom: '10px'
+                        }}
+                    >
+                        <Alert severity="error">Error - Not a domain, IP or IP range!</Alert>
+                    </div>
+                )}
             </form>
         </div>
     )
