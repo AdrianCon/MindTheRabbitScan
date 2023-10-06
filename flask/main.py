91a9d92e7e9a723eafd01a26675cb09895846a05
AdrianCon <adrian.dev@tutanota.com>
2023-10-06 03:20:32 +0000

    Change API route + CORS config

--- flask/main.py
+++ flask/main.py
@@ -1,11 +1,20 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS  # Import Flask-CORS
+from flask_cors import CORS, cross_origin  # Import Flask-CORS
 import nmap
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for your Flask app
-
-@app.route('/scan', methods=['POST'])
+#CORS(app)  # Enable CORS for your Flask app
+cors = CORS(app, resource={
+    r"/*":{
+        "origins":"*"
+    }
+})
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['FORCE_HTTPS'] = True
+app.config['CORS_HEADERS'] = 'Content-Type'
+@app.route('/api/scan', methods=['POST'])
+@cross_origin()
 def nmap_scan():
     try:
         ip_address = request.json['ip_address']
@@ -16,7 +25,9 @@ def nmap_scan():
         nm = nmap.PortScanner()
         # Run the Nmap scan
         scan_results = nm.scan(ip_address, arguments=f"-T4 -F {scan_type} {flags}")
-        return jsonify(scan_results)
+        response = jsonify(scan_results)
+        #response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
     except Exception as e:
         return jsonify({"error": str(e)})
 
