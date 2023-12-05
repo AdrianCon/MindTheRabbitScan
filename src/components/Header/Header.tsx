87ee748ea9eb76d36553d940c8e48a7870c67213
adriancon <adrian.c@adestech.mx>
2023-12-05 17:01:19 -0600

    Static header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,15 +3,16 @@ import GlitchText from '../GlitchText/GlitchText'
 import StairsText from '../StairsText/StairsText'
 import './Header.scss'
 export default function Header(){
-    const randomInt = Math.floor(Math.random() * (2 - 1 + 1) + 1)
+    // const randomInt = Math.floor(Math.random() * (2 - 1 + 1) + 1)
 
     return(
         <header className="head">
-            {randomInt === 1 ? 
+            {/* {randomInt === 1 ? 
             <GlitchText />
             :
             <StairsText />
-            }
+            } */}
+            <StairsText/>
         </header>
     )
 }
